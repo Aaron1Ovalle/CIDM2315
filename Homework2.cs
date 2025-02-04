62e276a12663b2d4496e12d80a741d16ffe65f49
Aaron1Ovalle <aaovalle1@buffs.wtamu.edu>
2025-02-03 23:53:54 -0600

    Homework2 code file

---
+++ Homework2.cs
@@ -0,0 +1,33 @@
+﻿namespace Lecture2;
+class Program
+{
+    static void Main(string[] args)
+    {
+         Console.Write("Enter a letter grade: ");
+        string grade = Console.ReadLine(); grade.ToUpper();
+        switch (grade)
+        {
+            case "A":
+                Console.WriteLine("GPA: 4");
+                break;
+            case "B":
+                Console.WriteLine("GPA: 3");
+                break;
+            case "C":
+                Console.WriteLine("GPA: 2");
+                break;
+            case "D":
+                Console.WriteLine("GPA: 1");
+                break;
+            case "F":
+                Console.WriteLine("GPA: 0");
+                break;
+            default:
+                Console.WriteLine("Wrong Letter Grade!");
+                break;
+        }
+    }
+}         
+           
+
+
